37e3eb6ceb684bfa1bcf6b1aadd69e21dd2e1222
kaamncn <92039823+kaamncn@users.noreply.github.com>
2022-10-04 15:23:00 +0700

    wk7

---
+++ workshop7.php
@@ -0,0 +1,18 @@
+<?php include "errordb.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="workshop7Insert.php" method="post">
+    username : <input type="text" name="username"><br><br>
+    password : <input type="text" name="password"><br><br>
+    name : <input type="text" name="name"><br><br>
+    address : <input type="text" name="address"><br><br>
+    mobile : <input type="text" name="mobile"><br><br>
+    email : <input type="text" name="email"><br><br>
+    <input type="submit" value="INSERT">
+    </form>
+</body>
+</html>
